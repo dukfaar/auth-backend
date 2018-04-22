c4ce07d9f5a1a8ba78a690c1812d1168ae09883b
Robert Schumann <dukfaar@gmail.com>
2018-04-22 09:21:53 +0200

    removed permission checks

--- src/graphql/PermissionError.ts
+++
@@ -1,6 +0,0 @@
-export default class PermissionError extends Error {
-    constructor(permission: string) {
-        super()
-        this.message = `Permission '${permission}' is missing`
-    }
-}
--- src/graphql/mutationResolvers.ts
+++ src/graphql/mutationResolvers.ts
@@ -14,22 +14,20 @@ import {
     Role
 } from '../model'
 
-import withAuth from './withAuth'
-
 export default {
-    createClient: withAuth('client create', (value, params) => createType(Client, {clientId: params.clientId}, params)),  
-    updateClient: withAuth('client update', (value, params) => updateType(Client, {clientId: params.clientId}, params)),
-    deleteClient: withAuth('client delete', (value, params) => Client.remove({clientId: params.clientId}).exec()),
+    createClient: (value, params) => createType(Client, {clientId: params.clientId}, params),  
+    updateClient: (value, params) => updateType(Client, {clientId: params.clientId}, params),
+    deleteClient: (value, params) => Client.remove({clientId: params.clientId}).exec(),
 
-    createUser: withAuth('user create', (value, params) => createType(User, {username: params.username}, params)),  
-    updateUser: withAuth('user update', (value, params) => updateType(User, {username: params.username}, params)),
-    deleteUser: withAuth('user delete', (value, params) => User.remove({username: params.usernam}).exec()),
+    createUser: (value, params) => createType(User, {username: params.username}, params),  
+    updateUser: (value, params) => updateType(User, {username: params.username}, params),
+    deleteUser: (value, params) => User.remove({username: params.usernam}).exec(),
 
-    createPermission: withAuth('permission create', (value, params) => createType(Permission, {name: params.name}, params)),  
-    updatePermission: withAuth('permission update', (value, params) => updateType(Permission, {name: params.name}, params)),
-    deletePermission: withAuth('permission delete', (value, params) => Permission.remove({username: params.name}).exec()),
+    createPermission: (value, params) => createType(Permission, {name: params.name}, params),  
+    updatePermission: (value, params) => updateType(Permission, {name: params.name}, params),
+    deletePermission: (value, params) => Permission.remove({username: params.name}).exec(),
 
-    createRole: withAuth('role create', (value, params) => createType(Role, {name: params.name}, params)),  
-    updateRole: withAuth('role update', (value, params) => updateType(Role, {name: params.name}, params)),
-    deleteRole: withAuth('role delete', (value, params) => Role.remove({name: params.name}).exec()),
+    createRole: (value, params) => createType(Role, {name: params.name}, params),  
+    updateRole: (value, params) => updateType(Role, {name: params.name}, params),
+    deleteRole: (value, params) => Role.remove({name: params.name}).exec(),
 }  
