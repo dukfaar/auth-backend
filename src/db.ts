b78959693cac3f6941b15e6ad9837d53d8462676
Robert Schumann <dukfaar@gmail.com>
2019-04-13 15:49:19 +0200

    ran update

--- src/db.ts
+++ src/db.ts
@@ -3,4 +3,5 @@ import * as mongoose from 'mongoose'
 let portString = process.env.DB_PORT ? ':' + process.env.DB_PORT : ''
 
 mongoose.connect(`mongodb://${process.env.DB_HOST || 'db'}${portString}/auth`, {
+  useNewUrlParser: true
 })
